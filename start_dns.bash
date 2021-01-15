98352384075b11b88b82aaec7fe4410529ca58ea
David Maier <david.maier@redislabs.com>
2021-01-15 21:57:19 +0100

    Improved to run it on MacOS and Linux

--- start_dns.bash
+++ start_dns.bash
@@ -2,9 +2,25 @@
 source ./settings.bash
 
 ## Prepare directories
-mkdir /srv/docker
-mkdir /srv/docker/bind
-chgrp docker /srv/docker/bind
+PLATFORM=`uname`
+
+# The default volume path on Linux
+VOLUME_PATH=/srv/docker
+
+if [[ "$PLATFORM" == 'Darwin' ]]
+then
+   # Let's assume that we don't have access to /srv,
+   # so using the user's folder as the parent folder
+   VOLUME_PATH=$HOME/srv/docker
+fi
+
+mkdir -p $VOLUME_PATH/bind
+
+
+if [[ "$PLATFORM" == 'Linux' ]]
+then
+  chgrp docker $VOLUME_PATH/bind
+fi
 
 ## Calculate the IP
 net_ip_pre=`echo $NET_CIDR | cut -f 1 -d'/' | cut -f 1-3 -d'.'`
@@ -15,5 +31,5 @@ echo ip = $net_ip
 docker run --name bind -d --restart=always \
   --network $NET_NAME --ip $net_ip \
   --publish 10000:10000/tcp \
-  --volume /srv/docker/bind:/data \
+  --volume $VOLUME_PATH/bind:/data \
   sameersbn/bind:9.10.3-20180127
